485f6a42a1ba7b3e59e10e9f1a08b314e9b6cfa1
byt3h3ad <adhirajd007@gmail.com>
2025-07-16 01:19:29 +0530

    fix: update resume link

--- src/data.ts
+++ src/data.ts
@@ -13,8 +13,7 @@ export const metadata = {
     movies: "https://trakt.tv/users/bytehead",
     music: "https://www.last.fm/user/notbytehead",
     links: "https://raindrop.io/adhiraj",
-    resume:
-      "https://drive.google.com/file/d/1Y0mkUytuV1nXixLeiftlb_1rPFtN4tfw/view",
+    resume: "https://byt3h3ad.github.io/resume.pdf",
   },
 };
 
